2cc2e5f2c996d48f2ac1d502cee8ff015c75216f
wqlcxm <147910015+wqlcxm@users.noreply.github.com>
2023-10-15 22:46:47 +0300

    Add files via upload

---
+++ modules.bat
@@ -0,0 +1,13 @@
+pip install requests
+pip install bs4
+pip install os
+pip install sys
+pip install folium
+pip install threading
+pip install time
+pip install random
+pip install datetime
+pip install sys
+pip install argparse
+pip install colorama
+ls
